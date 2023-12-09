ee0a440ac30b15043e801909107db11e832e52a4
Jim Aho <jaho1@volvocars.com>
2023-12-09 12:44:58 +0100

    feat(react): add initial app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Steady Coding</h1>
+        <a
+          className="App-link"
+          href="https://blog.steadycoding.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          blog.steadycoding.com
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
